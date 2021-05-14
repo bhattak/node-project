2c9c598900789e9590bfb6bf3450c6cbda7c9875
bhattak <70128340+bhattak@users.noreply.github.com>
2021-05-14 08:10:54 +0545

    messing up

--- index.js
+++ index.js
@@ -1,25 +1,29 @@
-// import fs, { readFile } from 'fs';
-// import readline from 'readline'
-// import linereader from 'line-reader'
+import fs, { readFile } from 'fs';
+import readline from 'readline'
+import linereader from 'line-reader'
 
-// const readInterface= readline.createInterface({
-//     input:fs.createReadStream('./data.txt'),
-//     output:process.stdout,
-//     console:false
-// })
-// readInterface.on('line',(line)=>{
-//     console.log(line)
-// })
+const readInterface= readline.createInterface({
+    input:fs.createReadStream('./data.txt'),
+    output:process.stdout,
+    console:false
+})
 
+readInterface.on('line',(line)=>{
+    console.log(line)
+})
+linereader.eachLine('./data.txt',(line)=>{
+    line.match('\d{4}-\d{2}-\d{2}')
+        console.log(line)
+})
 
+// import readline from "linebyline";
+// const rl = readline('./data.txt');
 
-import readline from "linebyline";
-const rl = readline('./data.txt');
-
-rl.on('line', function (line) {
-  if (line.includes("[error]") ){
-      console.log( line);
-  }
-}).on('error', function(e) {
-    console.log("Error !!!! ",e)
-});
+// rl.on('line', function (line) {
+//   if (line.includes("[error]") ){
+//       console.log( line);
+//   }
+  
+// }).on('error', function(e) {
+//     console.log("Error !!!! ",e)
+// });
